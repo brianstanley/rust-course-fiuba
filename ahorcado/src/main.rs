9e912edcb6e85ef1689bc05bcf5da59ca8837020
Brian <brianelblog@gmail.com>
2021-09-29 12:56:39 -0300

    store used letters

--- ahorcado/src/main.rs
+++ ahorcado/src/main.rs
@@ -41,7 +41,7 @@ fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
     Ok(io::BufReader::new(file).lines())
 }
 
-fn print_wrong_characters(hash_wrong_letters: HashMap<char, ()>) -> () {
+fn print_wrong_characters(hash_wrong_letters: &HashMap<char, ()>) -> () {
     let characters = hash_wrong_letters.keys().map(|s| s.to_string()).collect::<Vec<_>>().join(", ");
     println!("{}", characters);
 }
@@ -50,12 +50,19 @@ fn do_turn(guess_word: &String) {
     let mut remaining_attempts = 5;
     let mut guessed: Vec<String> = vec![String::from("_"); guess_word.len()];
     println!("Siguiente palabra");
-    let mut bad_letters = HashMap::new();
+    let mut bad_chars = HashMap::new();
+    let mut used_chars = HashMap::new();
 
     for _i in 0..5 {
         println!("Ingresa una letra");
-        let input_letter = get_letter();
+        let mut input_letter = get_letter();
         let mut found: bool = false;
+
+        while used_chars.contains_key(&input_letter) {
+            println!("La letra ingreada ya fue utilizada.");
+            println!("Ingresa una letra");
+            input_letter = get_letter();
+        }
 
         for (i, c) in (guess_word).chars().enumerate() {
             if c == input_letter {
@@ -64,10 +71,12 @@ fn do_turn(guess_word: &String) {
             }
         }
 
+        used_chars.insert(input_letter, ());
+
         if found {
             println!("Adivinaste las siguientes letras: {}", input_letter);
         } else {
-            bad_letters.insert(input_letter, ());
+            bad_chars.insert(input_letter, ());
         }
 
         if guessed.join("") == *guess_word {
@@ -77,8 +86,7 @@ fn do_turn(guess_word: &String) {
         } else if remaining_attempts == 1 {
             println!("Perdio. La palabra era {}", guess_word);
             println!("Letras mal utilizadas");
-            print_wrong_characters(bad_letters);
-            break;
+            print_wrong_characters(&bad_chars);
         } else {
             println!("La palabra hasta el momento es: ");
             print_word(&guessed);
